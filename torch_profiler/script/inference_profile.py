e7c77e747569c921ff6fc6433e7250a1e7c2082f
Monil Shah <monilshah@Monils-MacBook-Pro.local>
2026-05-26 18:38:19 -0700

    fix sys.path depth for script subdir

--- torch_profiler/script/inference_profile.py
+++ torch_profiler/script/inference_profile.py
@@ -9,7 +9,7 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'scripts'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'scripts'))
 
 import torch
 from torch.profiler import profile, ProfilerActivity
